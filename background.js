72da067ec439d2c54baa1231683c7b54256fb40f
Davi Freire <fsdavi.dev@gmail.com>
2024-06-18 23:14:29 -0300

    Create the daily alarm function to trigger the new tab with tangerino

--- background.js
+++ background.js
@@ -1,1 +1,29 @@
-chrome.alarms.create("Oxenteeeee", {delayInMinutes: 1});
+function setDailyAlarm(hour, minute) {
+  let now = new Date();
+  let alarmTime = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate(),
+    hour,
+    minute,
+    0,
+    0
+  );
+
+  if (alarmTime <= now) {
+    alarmTime.setDate(alarmTime.getDate() + 1);
+  }
+  let delay = alarmTime.getTime() - now.getTime();
+
+  chrome.alarms.create(hour + '-' + minute + 'Alarm', { when: Date.now() + delay });
+}
+
+setDailyAlarm(9, 0);
+setDailyAlarm(12, 0);
+setDailyAlarm(13, 0);
+setDailyAlarm(18, 0);
+
+chrome.alarms.onAlarm.addListener(function(alarm) {
+
+  chrome.tabs.create({ url: "https://app.tangerino.com.br/Tangerino/pages/LoginPage" });
+});
